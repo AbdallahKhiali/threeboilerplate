7a18faccca8b6589f10ecafc7f9e833195c1ae63
AbdallahKhiali <work.khiali@gmail.com>
2023-02-15 11:57:26 +0100

    boilerplate

--- main.js
+++ main.js
@@ -1,23 +1,66 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import vertexShader from "./shaders/vertexShader.glsl"
+import fragmentShader from "./shaders/fragmentShader.glsl"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector('#c');
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+
+// Set canvas size based on window size
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+const fov = 45;
+const aspect = window.devicePixelRatio; // the canvas default
+const near = 0.1;
+const far = 5;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.PlaneGeometry(boxWidth, boxHeight, boxDepth);
+
+const material = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+});
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
+
+function render(time) {
+  // convert time to seconds
+  // time *= 0.001; //
+
+
+  // Call resize function to make the canvas size dependent on the window size
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
+
+// Add resize event listener
+window.addEventListener('resize', () => {
+  resizeRendererToDisplaySize(renderer);
+});
